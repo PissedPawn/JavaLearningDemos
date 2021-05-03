79dd9fcdbfcb2a60a105c6fa3b14f4f2396b353d
Vuqar Rahim <vuqarahim@gmail.com>
2021-05-03 14:11:48 +0400

    Fixed Mernis Bug, Working Properly

--- InterfacesAndAbstractClassesProject/src/interfaceAndAbstract/MernisServiceAdapter.java
+++ InterfacesAndAbstractClassesProject/src/interfaceAndAbstract/MernisServiceAdapter.java
@@ -1,26 +1,22 @@
 package interfaceAndAbstract;
 
+import java.rmi.RemoteException;
 
-
-
-
-//import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
 public class MernisServiceAdapter implements ICustomerCheckService {
 
 	@Override
-	public boolean CheckIfReal(Customer customer) {
+	public boolean CheckIfReal(Customer customer) throws RemoteException {
 
-		//KPSPublicSoapProxy kpsPublic = new KPSPublicSoapProxy();
+		KPSPublicSoapProxy kpsPublic = new KPSPublicSoapProxy();
 
-		//boolean result = kpsPublic.TCKimlikNoDogrula(customer.getIdentityNo(),
-		//		customer.getFirstName().toUpperCase(), customer.getLastName().toUpperCase(), customer.getYearOfBirth());
-		
-		
+		boolean result = kpsPublic.TCKimlikNoDogrula(customer.getIdentityNo(), customer.getFirstName().toUpperCase(),
+				customer.getLastName().toUpperCase(), customer.getYearOfBirth());
 
-		//if (result)
-		//	return true;
-		//else
+		if (result)
+			return true;
+		else
 
 			return false;
 	}
